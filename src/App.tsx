b607ef3a8062652b6e2244dd2e53538b15f9f642
Alexey <algoritm211@gmail.com>
2021-10-10 22:03:16 +0300

    Add transforming code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
+import * as esbuild from 'esbuild-wasm';
 
 const App: React.FC = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+  const esBuildService = useRef<any>(null);
+
+  const initializeESBuild = async () => {
+    if (esBuildService.current !== null) {
+      return;
+    }
+    await esbuild.initialize({
+      worker: true,
+      wasmURL: './esbuild.wasm'
+    })
+    esBuildService.current = true
+  }
+
+  useEffect(() => {
+    initializeESBuild()
+  }, [])
+
+  const onSubmitCode = async () => {
+    if (!esBuildService.current) {
+      return alert('Service is not initialized try again in  5 seconds');
+    }
+    const result = await esbuild.transform(input, {
+      target: 'es2015',
+      loader: 'jsx',
+    })
+    setCode(result.code)
+  }
+
+  const onInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(event.target.value)
+  }
   return (
-    <div>
-      Code cloud
+    <div className='container'>
+      <h1>Code cloud</h1>
+      <textarea
+        onChange={onInputChange}
+        cols={30}
+        rows={10}>
+      </textarea>
+      <button onClick={onSubmitCode}>
+        Submit code
+      </button>
+      <pre>
+        {code}
+      </pre>
     </div>
   );
 }
