2a9cffc37cac2cd93a59d7a6980b65d7a2707960
Alexey <algoritm211@gmail.com>
2021-10-14 20:26:46 +0300

    Refactor onResolve function

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,22 +10,25 @@ export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({filter: /.*/}, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return {path: args.path, namespace: 'a'};
-        }
 
-        if (args.path.includes('./') || args.path.includes('../')){
-          const path = new URL(
-            args.path,
-            `https://unpkg.com${args.resolveDir}/`).href
-          return {
-            path: path,
-            namespace: 'a'
-          }
-        }
+      // Handling the main index.js file (main entry file)
+      build.onResolve({filter: /^index\.js$/}, () => {
+        return {path: 'index.js', namespace: 'a'};
+      })
 
+      // Handling nesting modules (relative paths in modules)
+      build.onResolve({filter: /^\.+\//}, (args) => {
+        const path = new URL(
+          args.path,
+          `https://unpkg.com${args.resolveDir}/`).href
+        return {
+          path: path,
+          namespace: 'a'
+        }
+      })
+
+      // Handling main file of module
+      build.onResolve({filter: /.*/}, async (args) => {
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
