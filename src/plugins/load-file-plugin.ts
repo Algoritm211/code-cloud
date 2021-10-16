a68f984ff5ab09e595baecc5c1db839946fee885
Alexey <algoritm211@gmail.com>
2021-10-16 17:00:28 +0300

    Add loader for css and refactor loadFilePlugin

--- src/plugins/load-file-plugin.ts
+++ src/plugins/load-file-plugin.ts
@@ -10,21 +10,50 @@ export const loadFilePlugin = (inputCode: string) => {
   return {
     name: 'load-file-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({filter: /.*/}, async (args) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
+      //Loading main file
+      build.onLoad({filter: /^index\.js$/}, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      })
 
+      // Loading caching result, if exists - going to next onLoad
+      build.onLoad({filter: /.*/}, async (args) => {
         const cachedData = await fileCacher.getItem<esbuild.OnLoadResult>(args.path)
 
         if (cachedData) {
           return cachedData
         }
+      })
 
-        const {data, request} = await axios.get(args.path)
+      // Loading css files
+      build.onLoad({filter: /.css$/}, async (args) => {
+        const {data, request} = await axios.get<string>(args.path)
+
+        const escapedCSS = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'")
+
+        const contents = `
+           const style = document.createElement('style')
+           style.innerText = '${escapedCSS}'
+           document.head.appendChild(style)
+          `
+        const result: esbuild.OnLoadResult =  {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname
+        }
+
+        await fileCacher.setItem(args.path, result)
+        return result
+      })
+
+      // Loading js and jsx files
+      build.onLoad({filter: /.*/}, async (args) => {
+        const {data, request} = await axios.get<string>(args.path)
 
         const result: esbuild.OnLoadResult =  {
           loader: 'jsx',
